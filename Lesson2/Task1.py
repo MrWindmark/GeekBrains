a719853fcc31e7c207d615ef5888d1e3c987c9fa
MrWindmark <agentstorm@ya.ru>
2020-06-21 21:15:48 +0300

    Revert "Merge branches 'Lesson1', 'Lesson2', 'Lesson3' and 'Lesson4' of https://github.com/MrWindmark/GeekBrains into Lesson4"
    This reverts commit fd962befab6f9e80a1ac203d366e9501e73ac8bf, reversing
    changes made to eceefa5e83697e1e30afa13bbc9d9cce328bbb11.

--- Lesson2/Task1.py
+++
@@ -1,9 +0,0 @@
-print('Первое задание')
-print('-'*15)
-print('Работа со списками\n')
-
-test_list = [1, 'first', True, 2.5, {'key': 'test_value'}, ('name', 'second name', "01.01.1900")]
-
-print(test_list)
-for step in test_list:
-    print(f'Тип: {type(step)} для значения \"{step}\"')
--- Lesson3/Task3.py
+++
@@ -1,9 +0,0 @@
-def my_func(arg1, arg2, arg3):
-    return arg1 + arg2 + arg3 - min(arg1, arg2, arg3)
-
-val_1 = 156
-val_2 = 154
-val_3 = 115
-
-print(my_func(val_1, val_2, val_3))
-print(my_func(4, 8, 6))
